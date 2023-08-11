780931d142f839f29f9497e34459a735e094447c
helmyandria <helmyandria@gmail.com>
2023-08-11 10:50:56 +0700

    first rabbitmq

---
+++ 1-First_RabbitMQ-App/Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var factory = new ConnectionFactory{HostName = "localhost"};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message Received: {message}");
+};
+
+channel.BasicConsume(queue: "letterbox", autoAck: true, consumer: consumer);
+
+Console.ReadKey();
---
+++ 1-First_RabbitMQ-App/Producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var factory = new ConnectionFactory{HostName = "localhost"};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+var message = "This is my first message";
+
+var encodeMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("","letterbox", null, encodeMessage);
+
+Console.WriteLine($"Published Message : {message}");
+
+Console.ReadKey();
