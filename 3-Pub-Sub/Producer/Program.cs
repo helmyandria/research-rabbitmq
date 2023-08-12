ac06e7e56b71fc2122b70b183a4c11c5bcf3c59b
helmyandria <helmyandria@gmail.com>
2023-08-12 14:42:24 +0700

    pub / sub

---
+++ 3-Pub-Sub/Producer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{HostName = "localhost"};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "pubsub", type: ExchangeType.Fanout);
+
+var message = "Hello I want to broadcast this message";
+
+var encodeMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange : "pubsub","", null, encodeMessage);
+
+Console.WriteLine($"Published Message : {message}");
