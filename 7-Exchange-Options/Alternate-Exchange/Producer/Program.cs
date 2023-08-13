3d2e46153756e658cfcbd09ed7835fee5d62e892
helmyandria <helmyandria@gmail.com>
2023-08-13 14:49:27 +0700

    alternate exchange, deadletter exchange, accept reject messages

---
+++ 7-Exchange-Options/Alternate-Exchange/Producer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{HostName = "localhost"};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "altexchange", 
+    type: ExchangeType.Fanout);
+
+channel.ExchangeDeclare(
+    exchange: "mainexchange", 
+    type: ExchangeType.Direct, 
+    arguments: new Dictionary<string, object>{
+        {"alternate-exchange", "altexchange"}
+});
+
+var message = "This is my first message";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("mainexchange","test", null, body);
+
+Console.WriteLine($"Published Message : {message}");
